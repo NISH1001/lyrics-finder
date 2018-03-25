a3a3b7dc4fe870d11eebeb36fccf6fded77923ba
NISH1001 <nishanpantha@gmail.com>
2018-03-25 19:50:06 +0545

    Fix parse error for the result div
    Fix parsing which was caused due to different type of divs for artistis,
    albums and songs.

--- lyrics.py
+++ lyrics.py
@@ -37,6 +37,7 @@ def search(url, query):
     response = urllib.request.urlopen(url_search)
     extractor = BeautifulSoup(response.read(), "html.parser")
     anchors = []
+    links = []
     try:
         table = extractor.find_all("table", {'class' : 'table'})[0]
         rows = table.find_all('tr')
@@ -49,7 +50,49 @@ def search(url, query):
     except ManualError as merr:
         merr.display()
         link = []
-    return links 
+    return links
+
+def search2(url, query):
+    """
+        Search the possible songs for this query.
+        Returns the list of url for the song.
+    """
+
+    # first encode
+    query = urlencode(query.lower())
+    url_query = "?q={}".format(query)
+    url_search = url + url_query
+    response = urllib.request.urlopen(url_search)
+    extractor = BeautifulSoup(response.read(), "html.parser")
+
+    anchors = []
+    links = []
+
+    # extract all the panels -> album, song, artist
+    # since the search can give 3 type of div (panel)
+    panels = extractor.find_all('div', {'class' : 'panel'})
+
+    # now find the panel containing list of all the songs
+    to_extract = ""
+    for panel in panels:
+        if re.search("song results", panel.text, re.IGNORECASE):
+            to_extract = panel
+            break
+
+    # if nothing found
+    if not to_extract:
+        links = []
+
+    else:
+        table = to_extract.find_all("table", {'class' : 'table'})[0]
+        rows = table.find_all('tr')
+        anchors = [ row.find('td').find('a').get('href')  for row in rows ]
+
+        # discard if the link/anchor is just a pagination link
+        links = [ anchor for anchor in anchors if not url_query in anchor ]
+
+    return links
+
 
 def lyrics_full(url):
     response = urllib.request.urlopen(url)
@@ -58,6 +101,10 @@ def lyrics_full(url):
     lyrics = soup.find_all("div", attrs={"class": None, "id": None})
     lyrics = [x.getText() for x in lyrics][0]
     return lyrics
+
+def select_song(links, topn=5):
+    inp = input("Select song ")
+    return int(inp)
 
 def main():
     args =  sys.argv
@@ -68,11 +115,15 @@ def main():
     if(len(args) > 1):
         query = ' '.join(args[1::])
         print("Searching...\nHave patience and be an awesome potato...")
-        links = search(url, query)
+        links = search2(url, query)
 
     if links:
+        print(links)
+        s =select_song(links, 4)
         lyrics = lyrics_full(links[0])
         print(lyrics)
+    else:
+        print("No songs found... -_-")
 
 if __name__ == "__main__":
     main()
